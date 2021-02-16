e4c18e41ea7a3fd5d7cab8d47e4b7a6602da72cd
dolphsoftware <38406426+dolphsoftware@users.noreply.github.com>
2021-02-16 01:10:48 +0100

    Working on test

--- Rader/app/src/main/java/com/sandbox/rader/ui/MainActivity.java
+++ Rader/app/src/main/java/com/sandbox/rader/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sandbox.rader.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.sandbox.rader.R;
 import com.sandbox.rader.app.App;
+import com.sandbox.rader.repository.UserRepository;
 import com.sandbox.rader.ui.dashboard_screen.DashBoardScreenFragment;
 import com.sandbox.rader.ui.login_screen.LoginFragment;
 import com.sandbox.rader.ui.registration_screen.RegistrationFragment;
@@ -15,10 +17,15 @@ import com.sandbox.rader.utils.Constants;
 
 public class MainActivity extends AppCompatActivity implements HomeHostCallback {
 
+
+    public UserRepository userRepository;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userRepository = UserRepository.getInstance();
         if (App.getUserManager().isLoggedIn()) {
             navigateToScreen(Constants.DASHBOARD_SCREEN);
         } else {
--- Rader/app/src/test/java/com/sandbox/rader/ExampleUnitTest.java
+++ Rader/app/src/test/java/com/sandbox/rader/ExampleUnitTest.java
@@ -1,17 +1,42 @@
 package com.sandbox.rader;
 
-import org.junit.Test;
+import android.content.Context;
 
-import static org.junit.Assert.*;
+import com.sandbox.rader.app.App;
+import com.sandbox.rader.ui.MainActivity;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+
 public class ExampleUnitTest {
+
+
+    private static final String FAKE_STRING = "Login was successful";
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void login() {
+        MainActivity testMain = new MainActivity();
+        testMain.userRepository.login("rade", "ribic");
+        boolean testLoggedIn = App.getUserManager().isLoggedIn();
+        assertTrue(testLoggedIn);
     }
+
+    @Test
+    public void testRegister() {
+        MainActivity testMain = new MainActivity();
+        testMain.userRepository.registerUser("dusan", "jovanovic");
+        boolean testLoggedIn = App.getUserManager().isLoggedIn();
+        assertTrue(testLoggedIn);
+    }
+
+
 }
